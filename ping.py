dc2a1527a424c537b30b837ce9a219de555da867
Matthew Louis Mauriello <mattm401@stanford.edu>
2021-03-24 15:59:35 -0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -5,6 +5,6 @@ Example of a Get Request that returns some information
 
 import requests
 
-x = requests.get('http://go.eecis.udel.edu:8081/api/v1/status/')
+x = requests.get('http://<ADD_URL>:8081/api/v1/status/')
 print(x.status_code)
 print(x.content)
